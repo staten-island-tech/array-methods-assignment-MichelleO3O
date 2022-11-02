3b5897aeee0b4c6ee9cccedd971646a84df43b35
MichelleO3O <mchao9406@gmail.com>
2022-11-02 14:32:43 -0400

    array 5

--- index.js
+++ index.js
@@ -4,47 +4,46 @@ const pokemon = [
     type: "Normal",
     weight: 14.3,
     category: "Evolution",
-    evolutionType: [
-      "water",
-      "electric",
-      "fire",
-      "ice",
-      "psychic",
-      "grass",
-      "dark",
-      "fairy",
-    ],
+    moves: ["Tackle", "Quick Attack", "Tail Whip"],
   },
   {
     name: "Mimikyu",
     type: "Ghost and Psychic",
     weight: 1.5,
     category: "Disguise",
+    moves: ["Shadow Claw", "Shadow Sneak", "Protect"],
   },
   {
     name: "Mienfoo",
     type: "Fighter",
     weight: 44.1,
     category: "Martial Arts",
+    moves: ["Fury Swipes", "Fake Out", "Quick Guard"],
   },
   {
     name: "Sudowoodo",
     type: "Rock",
     weight: 83.3,
     category: "Imitation",
+    moves: ["Fake Tears", "Wood Hammer", "Rock Slide"],
   },
   {
     name: "Torchic",
     type: "Fire",
     weight: 5.5,
     category: "Chick",
+    moves: ["Ember", "Flamethrower", "Aerial Ace"],
   },
 ];
 
 pokemon.forEach((pokemon) => {
   console.log(pokemon.name);
+  pokemon.moves.forEach((move) => {
+    console.log(move);
+  });
 });
 
 pokemon.forEach((pokemon) => {
-  console.log(pokemon);
+  const filteredMoves = pokemon.moves.filter((move) => move.length > 5);
+  console.log(filteredMoves);
 });
