1fc800dbcbae7b3df25a5ebd2c1d6d9a825f9a3b
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-11-02 10:18:17 -0400

    array 4

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-const pokemon = [{
-    name:];
-console.log(arr[0]);
+const pokemon = [
+  {
+    name: "Eevee",
+    type: "Normal",
+    weight: 14.3,
+    category: "Evolution",
+    evolutionType: [
+      "water",
+      "electric",
+      "fire",
+      "ice",
+      "psychic",
+      "grass",
+      "dark",
+      "fairy",
+    ],
+  },
+  {
+    name: "Mimikyu",
+    type: "Ghost and Psychic",
+    weight: 1.5,
+    category: "Disguise",
+  },
+  {
+    name: "Mienfoo",
+    type: "Fighter",
+    weight: 44.1,
+    category: "Martial Arts",
+  },
+  {
+    name: "Sudowoodo",
+    type: "Rock",
+    weight: 83.3,
+    category: "Imitation",
+  },
+  {
+    name: "Torchic",
+    type: "Fire",
+    weight: 5.5,
+    category: "Chick",
+  },
+];
+
+pokemon.forEach((pokemon) => {
+  console.log(pokemon.name);
+});
+
+pokemon.forEach((pokemon) => {
+  console.log(pokemon);
+});
